a91e228aa91489853e0c396ac0f354256f59ce32
taffyrooster <55558706+taffyrooster@users.noreply.github.com>
2022-07-22 15:14:01 -0400

    Update BLI_functions.py

--- BLI_functions.py
+++ BLI_functions.py
@@ -71,12 +71,12 @@ def preprocess(data,t_b,t_l,t_w,t_a,t_d):
 # =============================================================================
 # curvefit all sensors + steps
 # =============================================================================
-def fitAll(data_dict,fits,time_bounds,sensors,functions):
+def fitAll(data_dict,steps,time_bounds,sensors,functions):
     '''
     fitAll: fit every step in an BLI experiment
     Input:
         data_dict: a dictionary of data broken down by steps (from preprocess)
-        fits: an array of steps to fit
+        steps: an array of steps to fit
         time_bounds: an array of dimension (# sensors, # fits) with upper time limits,
                                             in case of baseline drift
         sensors: an array of sensors to fit
@@ -84,10 +84,10 @@ def fitAll(data_dict,fits,time_bounds,sensors,functions):
     Output:
         parameters_dict: fit parameters (Y0, Plateau, K) and their standard deviations
     '''
-    parameters_dict = dict(zip(sensors,[pd.DataFrame(index=fits,
+    parameters_dict = dict(zip(sensors,[pd.DataFrame(index=steps,
                       columns=['Y0','Plateau','K','Y0_std','Plateau_std','K_std']) for s in sensors]))
     
-    for i,step in enumerate(fits):
+    for i,step in enumerate(steps):
         for j,sensor in enumerate(sensors):
             func = functions[i]
             time_bound = time_bounds[i,j]
@@ -156,12 +156,12 @@ def plot_fit(data_dict,step,sensor,func,popt,time_bound=None,ax=None,**kwargs):
         ax.scatter(raw_data['Time0'],raw_data[sensor],s=1,**kwargs)
         return ax
 
-def plot_fit_all(data_dict,fits,time_bounds,sensors,functions,parameters_dict,plot_size=(4,4)):
+def plot_fit_all(data_dict,steps,time_bounds,sensors,functions,parameters_dict,plot_size=(4,4)):
     '''
     plot_fit_all: plot an entire BLI experiment with its fits
     Input:
         data_dict: a dictionary of data broken down by steps (from preprocess)
-        fits: which steps to plot
+        steps: which steps to plot
         time_bounds: an array of dimension (# sensors, # fits) with upper time limits,
                                             in case of baseline drift
         sensors: which sensor(s) to fit, defaults to all
@@ -172,12 +172,12 @@ def plot_fit_all(data_dict,fits,time_bounds,sensors,functions,parameters_dict,pl
     Output:
         None
     '''
-    plt.figure(figsize=(plot_size[0]*len(sensors), plot_size[1]*len(fits)))
+    plt.figure(figsize=(plot_size[0]*len(sensors), plot_size[1]*len(steps)))
     # plt.ioff()
-    for i,step in enumerate(fits):
+    for i,step in enumerate(steps):
         for j,sensor in enumerate(sensors):
-            ax = plt.subplot(len(fits),len(sensors),i*len(sensors)+j+1)
-            if i == (len(fits)-1):
+            ax = plt.subplot(len(steps),len(sensors),i*len(sensors)+j+1)
+            if i == (len(steps)-1):
                 ax.set_xlabel(sensor.split('_')[0])
             if j == 0:
                 ax.set_ylabel(step)
@@ -250,6 +250,97 @@ def get_curve_fit(data_dict,step,sensor,func,time_bound=None,**kwargs):
         perr = np.array([0,0,0])
     return popt, perr
 
+# =============================================================================
+# get summary of statistics + plot fit parameters
+# =============================================================================
+def get_parameters_stats(parameters_dict,sensors,concs):
+    '''
+    get_parameters_stats: get average and standard deviation from fit parameters
+    Input:
+        parameter_dict: parameters from fitAll
+        sensors: which sensor(s) to fit, defaults to all
+        concs:concentrations of association steps
+    Output:
+        parameters_consolidated: a dataframe with all association and disassociation averages and standard deviations
+    '''
+    parameters = ['Y0','Plateau','K']
+    parameters_consolidated = pd.DataFrame(index=['Assoc_' + p for p in parameters] + ['Disassoc_' + p for p in parameters],
+                                           columns=sensors)
+    for i,sensor in enumerate(sensors):
+        for j,parameter in enumerate(parameters):
+            d = parameters_dict[sensor][parameter]
+            assoc = d.loc[(d.index.str.contains('Assoc'))]
+            assoc = assoc / concs
+            assoc = assoc.loc[assoc != 0]
+            
+            parameters_consolidated.loc['Assoc_'+parameter,sensor]=np.average(assoc)
+            parameters_consolidated.loc['Assoc_'+parameter+'_std',sensor]=np.std(assoc)
+            
+            disassoc = d.loc[(d.index.str.contains('Disassoc'))]
+            disassoc = disassoc.loc[disassoc != 0]
+            parameters_consolidated.loc['Disassoc_'+parameter,sensor]=np.average(disassoc)
+            parameters_consolidated.loc['Disassoc_'+parameter+'_std',sensor]=np.std(disassoc)
+            if parameter == 'K':
+                if np.average(assoc) == 0:
+                    parameters_consolidated.loc['Kd',sensor]=0
+                    parameters_consolidated.loc['Kd_std',sensor]=0
+                else:
+                    parameters_consolidated.loc['Kd_K',sensor]=np.average(disassoc)/np.average(assoc)
+                    parameters_consolidated.loc['Kd_K_std',sensor]=np.average(disassoc)/np.average(assoc)*np.sqrt(
+                        (np.std(disassoc)/np.average(disassoc))**2+(np.std(assoc)/np.average(assoc))**2)
+    
+    return parameters_consolidated
+
+def plot_fit_parameters(parameters_dict,steps,sensors,time_bounds,functions,concs,parameters_to_plot=['K'],**kwargs):
+    '''
+    plot_fit_parameters: plot an entire BLI experiment with its fits
+    Input:
+        parameter_dict: parameters from fitAll
+        steps: which steps to plot
+        sensors: which sensor(s) to fit
+        time_bounds: an array of dimension (# sensors, # fits) with upper time limits,
+                                            in case of baseline drift
+        functions: which functions correspond to which steps
+        concs: concentrations of steps, only for association phases
+        parameters_to_plot: which parameters to plot, defaults to K
+        **kwargs: kwargs for plotting
+    Output:
+        None
+    '''
+    parameters_consolidated = get_parameters_stats(parameters_dict,sensors,concs)
+    
+    plt.figure()
+    for p,parameter in enumerate(parameters_to_plot):
+        if parameter == 'K':
+            for k,step in enumerate(['Assoc','Disassoc','Kd']):
+                ax = plt.subplot(len(['Assoc','Disassoc','Kd']),len(parameters_to_plot),3*p+k+1)
+                ax.set_ylabel(parameter+'_'+step)
+                plot_data = parameters_consolidated.loc[[step+'_'+parameter,step+'_'+parameter+'_std'],sensors]
+            
+                ax = plot_fit_parameter(plot_data,sensors,ax,**kwargs)
+        else:
+            #TODO: plot Y0 and plateau? not sure why'd you want to do that at this scale. maybe do later
+            break
+
+def plot_fit_parameter(plot_data,sensors,ax=None,**kwargs):
+    '''
+    plot_fit_parameter: plot an single BLI rate plot
+    Input:
+        plot_data: dataframe, subset of parameter_consolidated
+        sensors: which sensor(s) to fit
+        ax: ax object for subplotting, default to None
+        **kwargs: kwargs for plotting
+    Output:
+        None
+    '''
+    values = plot_data.loc[plot_data.index.str.contains('std')].values[0]
+    error = plot_data.loc[~(plot_data.index.str.contains('std'))].values[0]
+    if ax is None:
+        plt.bar(sensors,values,yerr=error,capsize=2,log=1)
+    else:
+        ax = plt.bar(sensors,values,yerr=error,capsize=2,log=1)
+        return ax
+    
 # =============================================================================
 # output data by timestep
 # =============================================================================
